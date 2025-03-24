8a93819881b6f79b7d7f1dae0d8cd618aee4a5cc
crazywhalecc <jesse2061@outlook.com>
2025-03-24 23:50:12 +0800

    Refactor, supports shared extension build now !

--- src/SPC/store/Downloader.php
+++ src/SPC/store/Downloader.php
@@ -123,10 +123,6 @@ class Downloader
             if (!$match_result) {
                 return $release['assets'];
             }
-            if ($source['match'] === 'Source code') {
-                $url = $release['tarball_url'];
-                break;
-            }
             foreach ($release['assets'] as $asset) {
                 if (preg_match('|' . $source['match'] . '|', $asset['name'])) {
                     $url = $asset['browser_download_url'];
@@ -139,9 +135,6 @@ class Downloader
             throw new DownloaderException("failed to find {$name} release metadata");
         }
         $filename = basename($url);
-        if ($source['match'] === 'Source code') {
-            $filename = $name . $filename . '.tar.gz';
-        }
 
         return [$url, $filename];
     }
